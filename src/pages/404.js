cb346508f3c6590f08e09a1efc56b164569c3c77
Natasha Williams <natashamary_williams@hotmail.co.uk>
2021-04-19 17:04:06 +0100

    Improvements to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Section from '../components/section'
 
 export default function NotFound() {
     return (
-        <Layout title="Page Not Found">
-            <p>Oops! The page you are looking for has been removed.</p>
+        <Layout>
+            <Section title="Page Not Found">
+                <p>Oops! The page you are looking for has been removed.</p>
+            </Section>
         </Layout>
     )
 }
